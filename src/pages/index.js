a59a49e319a8be1bcd9c8bf5922ad99755cd0980
Unwired.jp <>
2021-10-07 11:57:56 +0900

    555 deploy error

--- src/pages/index.js
+++ src/pages/index.js
@@ -39,7 +39,7 @@ export default () => (
             </figure>
             <h3>フルーツ</h3>
             <p>FRUIT</p>
-            <p>甘くてすっぱくておいしい果実たち。<br />旬のフルーツを満喫します。</p>
+            <p>甘くてすっぱくておいしい果実たち。<br />旬のフルーツを満喫します</p>
           </div>
           <div className="detail">
             <figure>
